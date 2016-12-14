70053ccca75a7c504f6a387e144c2a2b8426ef6b
sylvain <sylvain.tauxe@students.hevs.ch>
2016-12-14 11:01:23 +0100

    Weapon mapped

--- src/main/java/ch/hevs/businessobject/Weapon.java
+++ src/main/java/ch/hevs/businessobject/Weapon.java
@@ -1,5 +1,70 @@
 package ch.hevs.businessobject;
 
+import javax.persistence.*;
+
+@Entity
 public class Weapon {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	private int id;
+	private String name;
+	private int year_creation;
+	private int weight;
+	private int scope;
+	/*@OneToOne
+	@JoinColumn(name="country_fk", nullable=false)*/
+	//private Country country;
+	public Weapon(){
+		
+	}
+	
+	public Weapon(String name, int year, int weight, int scope){
+		this.name=name;
+		this.year_creation = year;
+		this.weight = weight;
+		this.scope = scope;
+		//this.country = country;
+	}
+	
+	/*public Country getCountry() {
+		return country;
+	}
+
+	public void setCountry(Country country) {
+		this.country = country;
+	}*/
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getYear_creation() {
+		return year_creation;
+	}
+	public void setYear_creation(int year_creation) {
+		this.year_creation = year_creation;
+	}
+	public int getWeight() {
+		return weight;
+	}
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+	public int getScope() {
+		return scope;
+	}
+	public void setScope(int scope) {
+		this.scope = scope;
+	}
+	
+	
 }
